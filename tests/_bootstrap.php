67550eb13ed90a91a7ede768e1efd5857059ea57
Victor Escobar González <victor@notwebdesign.com>
2015-07-08 16:39:27 +0200

    Adding missing classes.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,4 +2,4 @@
 // This is global bootstrap for autoloading
 
 include_once dirname(__FILE__) . '/_support/NenoExtension.php';
-include_once dirname(__FILE__) . '/_support/Recorder.php';
+include_once realpath(dirname(__FILE__) . '/../vendor/codeception/codeception/ext/Recorder.php');
