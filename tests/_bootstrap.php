39d99f31fe8f44eee0ef178125a192845130af7e
Victor Escobar González <victor@notwebdesign.com>
2015-07-08 15:13:16 +0200

    Finishing Neno installation test.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,3 +2,4 @@
 // This is global bootstrap for autoloading
 
 include_once dirname(__FILE__) . '/_support/NenoExtension.php';
+include_once dirname(__FILE__) . '/_support/Recorder.php';
--- tests/acceptance/01-InstallNenoCest.php
+++ tests/acceptance/01-InstallNenoCest.php
@@ -10,6 +10,7 @@ class InstallNenoCest
 {
 	public function installNeno(AcceptanceTester $I)
 	{
+		$I->maximizeWindow();
 		$I->am('Administrator');
 		$I->installJoomla();
 		$I->doAdministratorLogin();
@@ -29,7 +30,6 @@ class InstallNenoCest
 		// Installing Component
 		$I->installExtensionFromDirectory($path . 'com_neno');
 
-		/*
 		// Enabling plugin
 		$I->enablePlugin('Neno plugin');
 
@@ -40,24 +40,24 @@ class InstallNenoCest
 
 		// Get started Screen
 		$I->click('Get Started');
-		$I->waitForJS('return jQuery.active == 0', 20);
+		$I->waitForJS('return jQuery.active == 0', 5);
 		$I->wait(5);
 
 		// First step - Source language
-		$I->see('Next', 'button');
+		$I->see('Next');
 		$I->click(['xpath' => "//button[@type='button']"]);
-		$I->waitForJS('return jQuery.active == 0', 20);
+//		$I->waitForJS('return jQuery.active == 0', 5);
 		$I->wait(5);
 
 		// Second step - Translation methods
-		$I->see('Next');
-		$I->click(['xpath' => "//button[@type='button']"]);
-		$I->waitForJS('return jQuery.active == 0', 20);
-		$I->wait(5);
+		$I->canSee('Next');
+		$I->click('Next');
+		$I->waitForJS('return jQuery.active == 0', 5);
+		//$I->wait(5);
 
 		// Third step- Install language(s)
 		$I->click(['css' => "#add-languages-button"]);
-		$I->waitForJS('return jQuery.active == 0', 20);
+		$I->waitForJS('return jQuery.active == 0', 5);
 		$I->wait(5);
 		$I->waitForElementVisible(['css' => '#languages-modal']);
 		$I->click(['css' => "[data-language='de-DE']"]);
@@ -65,17 +65,21 @@ class InstallNenoCest
 		$I->wait(10);
 		$I->seeElement(['css' => ".loading-iso-de-DE"]);
 		$I->seeElement(['css' => ".loading-iso-es-ES"]);
-		$I->click("Close");
+		$I->see('Close');
+		$I->click(".close-button", '#languages-modal');
 		$I->click(['xpath' => "(//button[@type='button'])[4]"]);
 
 		// Fourth step- Installing Neno
 		$I->wait(5);
-		$I->click(['css' => "#backup-created-checkbox"]);
-		$I->click(['css' => "label.checkbox"]);
-		$I->click(['css' => "#proceed-button"]);
+		$I->click("#backup-created-checkbox");
+		$I->click("#proceed-button");
 
 		// Fifth step- Installing Neno has been accomplish successfully
-		$I->waitForElement("#submenu > li > a");
-		$I->doAdministratorLogout();*/
+		$I->wait(5);
+		$I->makeScreenshot('lol1');
+		\Codeception\Util\Debug::debug($I->getVisibleText());
+		$I->makeScreenshot('lol2');
+		$I->waitForElement("#submenu > li > a", 300);
+		$I->doAdministratorLogout();
 	}
 }
