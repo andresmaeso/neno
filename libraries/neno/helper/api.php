6e744e6b440c3b85cfeff217bdb38eefd2b29502
Victor Escobar González <victor@notwebdesign.com>
2015-07-29 15:46:01 +0200

    Fixing issue getting TC available amount.

--- libraries/neno/helper/api.php
+++ libraries/neno/helper/api.php
@@ -90,6 +90,7 @@ class NenoHelperApi
                 if ($apiResponse->headers['Content-Type'] === 'application/json')
                 {
                     $data = json_decode($data, true);
+                    $data = $data['response'];
                 }
 
                 $response = $data;
