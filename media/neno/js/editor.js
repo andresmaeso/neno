64d8f804bdb716f5aba5a41913c7fca58db2bf44
vistiyos <victor@notwebdesign.com>
2015-09-18 15:14:30 +0200

    Format HTML code.

--- media/neno/js/editor.js
+++ media/neno/js/editor.js
@@ -181,7 +181,7 @@ function initCodemirror() {
 	editors = {};
 
 	jQuery('.main-translation-div').each(function () {
-
+		addExtensions();
 		var translation_id = getTranslationId(jQuery(this));
 		var mode = 'htmlmixed';
 
@@ -208,6 +208,11 @@ function initCodemirror() {
 			matchTags     : {bothTags: true}
 		});
 
+		if (mode != 'application/ld+json') {
+			formatEditor(editor.left);
+			formatEditor(editor.right);
+		}
+
 		editors[translation_id] = editor;
 
 		editor.right.on('scroll', mirrorscroll);
@@ -233,6 +238,65 @@ function isJSON(string) {
 
 function prettyJSON(string) {
 	return JSON.stringify(JSON.parse(string), null, 4)
+}
+
+function formatEditor(editor) {
+	var totalLines = editor.lineCount();
+	var totalChars = editor.getTextArea().value.length;
+	editor.autoFormatRange({line: 0, ch: 0}, {line: totalLines, ch: totalChars});
+	editor.autoIndentRange({line: 0, ch: 0}, {line: totalLines, ch: totalChars});
+}
+
+function addExtensions() {
+	CodeMirror.defineExtension("autoFormatRange", function (from, to) {
+		var cm = this;
+		var outer = cm.getMode(), text = cm.getRange(from, to).split("\n");
+		var state = CodeMirror.copyState(outer, cm.getTokenAt(from).state);
+		var tabSize = cm.getOption("tabSize");
+
+		var out = "", lines = 0, atSol = from.ch == 0;
+
+		function newline() {
+			out += "\n";
+			atSol = true;
+			++lines;
+		}
+
+		for (var i = 0; i < text.length; ++i) {
+			var stream = new CodeMirror.StringStream(text[i], tabSize);
+			while (!stream.eol()) {
+				var inner = CodeMirror.innerMode(outer, state);
+				var style = outer.token(stream, state), cur = stream.current();
+				stream.start = stream.pos;
+				if (!atSol || /\S/.test(cur)) {
+					out += cur;
+					atSol = false;
+				}
+				if (!atSol && inner.mode.newlineAfterToken &&
+					inner.mode.newlineAfterToken(style, cur, stream.string.slice(stream.pos) || text[i + 1] || "", inner.state))
+					newline();
+			}
+			if (!stream.pos && outer.blankLine) outer.blankLine(state);
+			if (!atSol) newline();
+		}
+
+		cm.operation(function () {
+			cm.replaceRange(out, from, to);
+			for (var cur = from.line + 1, end = from.line + lines; cur <= end; ++cur)
+				cm.indentLine(cur, "smart");
+			//cm.setSelection(from, cm.getCursor(false));
+		});
+	});
+
+	// Applies automatic mode-aware indentation to the specified range
+	CodeMirror.defineExtension("autoIndentRange", function (from, to) {
+		var cmInstance = this;
+		this.operation(function () {
+			for (var i = from.line; i <= to.line; i++) {
+				cmInstance.indentLine(i, "smart");
+			}
+		});
+	});
 }
 
 
