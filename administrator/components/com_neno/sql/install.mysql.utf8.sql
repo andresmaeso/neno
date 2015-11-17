3876802db66da76b1eb70a275c7141ac61bc8cdf
vistiyos <victor@notwebdesign.com>
2015-11-17 16:42:50 +0100

    Fixing issue saving filters with the same field specified twice (Range filters).

--- administrator/components/com_neno/sql/install.mysql.utf8.sql
+++ administrator/components/com_neno/sql/install.mysql.utf8.sql
@@ -365,8 +365,7 @@ CREATE TABLE IF NOT EXISTS `#__neno_content_element_table_filters` (
 	`table_id`             INT(11)      NOT NULL,
 	`field_id`             INT(11)      NOT NULL,
 	`comparaison_operator` VARCHAR(45)  NOT NULL,
-	`filter_value`         VARCHAR(255) NOT NULL,
-	UNIQUE KEY `table_id` (`table_id`, `field_id`)
+	`filter_value`         VARCHAR(255) NOT NULL
 );
 
 INSERT IGNORE INTO `#__neno_machine_translation_apis` VALUES (1, 'Google', 'machine'), (3, 'Yandex', 'machine'), (4, 'Bing', 'machine');
